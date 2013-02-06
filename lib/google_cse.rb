0781e2eb439d81335c0b2908ebef7f608fd60dc4
Achilles Charmpilas <ac@humbuckercode.co.uk>
2013-02-07 00:24:05 +0100

    error handling

--- lib/google_cse.rb
+++ lib/google_cse.rb
@@ -3,20 +3,44 @@ require "google_cse/result"
 require "google_cse/version"
 
 module GoogleCSE
+  class MissingKey < StandardError
+    def initialize
+      super "You need to specify a Google CSE Key (GoogleCSE::KEY = 'my-key-goes-here')"
+    end
+  end
+  
+  class MissingCX < StandardError
+    def initialize
+      super "You need to specify a Google CSE CX (GoogleCSE::CX = 'my-cx-goes-here')"
+    end
+
+  end
+  
   def self.search query, params = {}
-    query(default_params.merge(:q => query).merge(params))
+    query({:q => query}.merge(params))
   end
   
   def self.image_search query, params = {}
-    query(default_params.merge(:searchType => :image, :q => query).merge(params))
-  end
-  
-  def self.default_params
-    { :cx => CX, :key => KEY }
+    query({:searchType => :image, :q => query}.merge(params))
   end
 
 private
   def self.query params
-    Query.new(:params => params)
+    begin
+      Query.new(:params => default_params.merge(params))
+    rescue NameError => e
+      case e.message
+      when 'uninitialized constant GoogleCSE::CX' 
+        raise MissingCX
+      when 'uninitialized constant GoogleCSE::KEY'
+        raise MissingKey
+      end
+    end
+    
+    
+  end
+  
+  def self.default_params
+    { :cx => CX, :key => KEY }
   end
 end
--- spec/google_cse_spec.rb
+++ spec/google_cse_spec.rb
@@ -3,34 +3,77 @@ require 'spec_helper'
 describe GoogleCSE do
   subject { GoogleCSE }
   
-  it { subject.search('lemmy').should be_a(GoogleCSE::Query) }
-  it { subject.image_search('lemmy').should be_a(GoogleCSE::Query) }
-  
-  describe '#search' do
-    subject { GoogleCSE.search('lemmy') }
+  describe 'without config' do
     before {
-      subject.stub(:fetch) { 
-        subject.response = JSON.parse(File.read('./spec/files/google_search.txt'))
-        subject.parse_response!
-        subject
-      }
+      GoogleCSE.send(:remove_const, :CX)
+      GoogleCSE.send(:remove_const, :KEY)
     }
     
-    it { subject.fetch.results.size.should eq(10) }
-    it { subject.fetch.results.first.link.should eq(subject.fetch.results.first['link']) }
+    after {
+      GoogleCSE::CX = 'cse-identifier'
+      GoogleCSE::KEY = 'custom-search-key'
+    }
+    
+    it { expect {subject.search('lemmy')}.to raise_error(GoogleCSE::MissingCX) }
+    it { expect {subject.image_search('lemmy')}.to raise_error(GoogleCSE::MissingCX) }
   end
   
-  describe '#image_search' do
-    subject { GoogleCSE.image_search('lemmy') }
+  describe 'with only key' do
     before {
-      subject.stub(:fetch) { 
-        subject.response = JSON.parse(File.read('./spec/files/google_image_search.txt'))
-        subject.parse_response!
-        subject
-      }
+      GoogleCSE.send(:remove_const, :CX)
     }
     
-    it { subject.fetch.results.size.should eq(10) }
-    it { subject.fetch.results.first.link.should eq(subject.fetch.results.first['link']) }
+    after {
+      GoogleCSE::CX = 'cse-identifier'
+    }
+    
+    it { expect {subject.search('lemmy')}.to raise_error(GoogleCSE::MissingCX) }
+    it { expect {subject.image_search('lemmy')}.to raise_error(GoogleCSE::MissingCX) }
+  end
+  
+  describe 'with only cx' do
+    before {
+      GoogleCSE.send(:remove_const, :KEY)
+    }
+    
+    after {
+      GoogleCSE::KEY = 'custom-search-key'
+    }
+    
+    it { expect {subject.search('lemmy')}.to raise_error(GoogleCSE::MissingKey) }
+    it { expect {subject.image_search('lemmy')}.to raise_error(GoogleCSE::MissingKey  ) }
+  end
+  
+  describe 'with config' do
+    it { subject.search('lemmy').should be_a(GoogleCSE::Query) }
+    it { subject.image_search('lemmy').should be_a(GoogleCSE::Query) }
+    
+    describe '#search' do
+      subject { GoogleCSE.search('lemmy') }
+      before {
+        subject.stub(:fetch) { 
+          subject.response = JSON.parse(File.read('./spec/files/google_search.txt'))
+          subject.parse_response!
+          subject
+        }
+      }
+    
+      it { subject.fetch.results.size.should eq(10) }
+      it { subject.fetch.results.first.link.should eq(subject.fetch.results.first['link']) }
+    end
+  
+    describe '#image_search' do
+      subject { GoogleCSE.image_search('lemmy') }
+      before {
+        subject.stub(:fetch) { 
+          subject.response = JSON.parse(File.read('./spec/files/google_image_search.txt'))
+          subject.parse_response!
+          subject
+        }
+      }
+    
+      it { subject.fetch.results.size.should eq(10) }
+      it { subject.fetch.results.first.link.should eq(subject.fetch.results.first['link']) }
+    end
   end
 end
